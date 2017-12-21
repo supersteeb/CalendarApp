c4a5c269736f91043bd510922171caa4955adf3a
= <nguyenvuonganhvy@gmail.com>
2017-12-21 22:14:04 +0700

    Finished render multiple calendar

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,23 @@
 class HomesController < ApplicationController
-	before_action :get_completed_lists, :set_default_todolist
+  before_action :set_default_todolist, only: [:index]
+	before_action :set_number_of_calendar, :get_completed_lists, only: [:index]
 
   def index
-  	@todoitems = @todolist.todoitems.order(:created_at)
+    h = Todolist.send("last_#{@n_o_calendar}_months")
+                .group_by {|l| l.created_at.beginning_of_month.strftime('%Y-%m-%d')}
+    month_start = Date.current.prev_month(@n_o_calendar - 1).month
+    month_end = Date.current.month
+    puts "\n\n\n Month Start: #{month_start} \n\n\n"
+    puts "\n\n\n Month End: #{month_end} \n\n\n"
+    @list_groups = Hash[(month_start..month_end).map { |month| [ "#{Date.current.year}-#{manipulate(month)}-01", 0 ] }].merge(h)
+  	@todoitems = @todolist.todoitems.order(:created_at) if current_user
   end
 
   private
+
+  def manipulate(month)
+    ('0' + month.to_s).last(2)
+  end
 
   def get_completed_lists
     @completed_lists = Todolist.where(percent: 100)
@@ -20,5 +32,10 @@ class HomesController < ApplicationController
 
   def set_default_todolist
       @todolist = Todolist.today.first || Todolist.create
-    end
+  end
+
+  def set_number_of_calendar
+    params[:multiple_calendar] ||= 1
+    @n_o_calendar =  params[:multiple_calendar].to_i
+  end
 end
